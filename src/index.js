fb818a488e54a4aa4097cde4b481f628e281f363
Douglas Camargo <camargo.douglas@icloud.com>
2020-02-26 19:58:34 -0300

    showing port

--- src/index.js
+++ src/index.js
@@ -19,5 +19,5 @@ app.use(morgan("dev"));
 app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp',  'uploads')));
 app.use(require("./routes"));
 var server = app.listen(process.env.PORT || 3001);
-var port = server.address().port;
-console.log("Express is working on port " + port);
+// var port = server.address().port;
+// console.log("Express is working on port " + port);
