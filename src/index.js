8eeef4933a11f99ce5e61ed0d5eccd2d3b7ab1a8
Douglas Camargo <camargo.douglas@icloud.com>
2020-02-26 23:12:59 -0300

    conection string

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const mongoose = require("mongoose");
 const path = require("path");
 
 const app = express();
-mongoose.connect("mongodb+srv://dkcamargox:camargo1508@cluster0-zmptz.gcp.mongodb.net/test?retryWrites=true&w=majority" , 
+mongoose.connect(process.env.MONGO_URL , 
     {
         useNewUrlParser: true,
         useUnifiedTopology: true
