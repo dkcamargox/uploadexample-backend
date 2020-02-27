09c2dd8c51ca2f4d8c6b9d32251dea54fded5995
Douglas Camargo <camargo.douglas@icloud.com>
2020-02-27 00:36:00 -0300

    trying console.log

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ mongoose.connect(process.env.MONGO_URL ,
     }
 );
 const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error: "));
+console.log(db);
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
